68989d15629f97acd0456eb40c9781afed87da66
Natallia_Dubrouskaya <natallia_dubrouskaya@epam.com>
2017-06-14 14:29:15 +0300

    homework #3 send response only on GET method

--- Homework#3/server.js
+++ Homework#3/server.js
@@ -6,45 +6,62 @@ const server = require('http').createServer();
 
 
 server.on('request', (request, response) => {
-    let promise = new Promise((resolve, reject) => {
-        const parsedUrl = url.parse(request.url, true);
-        const query = parsedUrl.query;
+    if(request.method === 'GET') {
+        let promise = new Promise((resolve, reject) => {
+            const parsedUrl = url.parse(request.url, true);
+            const query = parsedUrl.query;
 
-        const FILE_PATH = 'test.txt';
+            const FILE_PATH = 'test.txt';
 
-        if(parsedUrl.pathname === '/') {
-            fs.readFile(FILE_PATH, (err, data) => {
-                if (err) throw err;
-                return resolve(data);
-            });
-        } else if(parsedUrl.pathname === '/find') {
-            fs.readFile(FILE_PATH, (err, data) => {
-                if (err) throw err;
-                const lines = data.toString('utf8').split("\n"); // convert data to a string
-                return findWord(lines, query, resolve);
-            });
-        } else if(parsedUrl.pathname === '/show') {
-            fs.readFile(FILE_PATH, (err, data) => {
-                if (err) throw err;
-                const lines = data.toString('utf8').split("\n");
-                return showLines(lines, query, resolve, reject);
-            });
-        } else if(request.url === '/os') {
-            return resolve(`CPU: ${JSON.stringify(os.cpus())}\nFree Memory: ${(os.freemem() / 1024 / 1024 / 1024).toFixed(2)} Gb`);
-        } else {
-            return reject('404. Not found.\n');
-        }
-    });
-    promise
-        .then(data => {
-            response.write(data);
-        }, message => {
-            message && response.write(message);
-            response.write('Try next urls:\n/\n/find?str=some_word\n/show?startLine=1&startColumn=0\nshow?startLine=1&startColumn=0&endLine=11&endColumn=4\n/os');
-        })
-        .then(() => {
-            response.end();
+            if (parsedUrl.pathname === '/') {
+                fs.readFile(FILE_PATH, (err, data) => {
+                    if (err) throw err;
+                    return resolve(data);
+                });
+            } else if (parsedUrl.pathname === '/find') {
+                fs.readFile(FILE_PATH, (err, data) => {
+                    if (err) throw err;
+                    const lines = data.toString('utf8').split("\n"); // convert data to a string
+                    return findWord(lines, query, resolve);
+                });
+            } else if (parsedUrl.pathname === '/show') {
+                fs.readFile(FILE_PATH, (err, data) => {
+                    if (err) throw err;
+                    const lines = data.toString('utf8').split("\n");
+                    return showLines(lines, query, resolve, reject);
+                });
+            } else if (request.url === '/os') {
+                return resolve(`
+            OS type: ${os.type()}
+            OS platform: ${os.platform()}
+            CPU architecture: ${os.arch()}
+            CPU info: ${JSON.stringify(os.cpus())}
+            Total system memory: ${toGb(os.totalmem())} Gb
+            Free Memory: ${toGb(os.freemem())} Gb`);
+            } else {
+                return reject();
+            }
         });
+        promise
+            .then(data => {
+                response.write(data);
+            }, (error = {}) => {
+                response.writeHead(error.status || 404);
+                error.message && response.write(error.message);
+                response.write('Try next urls:\n' +
+                    '/ - show content of test.txt file\n' +
+                    '/find?str=some_word - find some_word in test.txt file\n' +
+                    '/show?startLine=1&startColumn=0 - cut and show a piece of file\n' +
+                    '/show?startLine=1&startColumn=0&endLine=11&endColumn=4\n' +
+                    '/os - show info about OS');
+            })
+            .then(() => {
+                response.end();
+            });
+    } else {
+        response.writeHead(405);
+        response.end();
+    }
 });
 
 server.listen(8000);
@@ -83,6 +100,10 @@ function showLines(lines, query, resolve, reject) {
         });
         return resolve(Buffer.from(result.join('\n')));
     } else {
-        return reject(`Test file has ${lines.length} lines. Please, enter correct line numbers (1-${lines.length})!\n`);
+        return reject({status: 400, message: `Test file has ${lines.length} lines. Please, enter correct line numbers (1-${lines.length})!\n`});
     }
+}
+
+function toGb(bytes) {
+    return (bytes / 1024 / 1024 / 1024).toFixed(2);
 }
